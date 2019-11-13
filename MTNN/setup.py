dc493ddb4900786a23604d5fb588e3c9093a0b8f
Mao, Christina <mao6@llnl.gov>
2019-11-12 18:05:48 -0800

    Adding setup.py

---
+++ MTNN/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name = "MTNN",
+      version = "0.1",
+      description = "Module to test multigrid training on neural networks",
+      author = "",
+      author_email = "",
+      # install_requires
+      packages = find_packages()
+      )
