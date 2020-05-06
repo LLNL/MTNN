208e8eecff6ece251180759ced1332c19718335d
Mao, Christina <mao6@llnl.gov>
2020-05-06 12:19:01 -0700

    Add MTNN/utils.py

--- MTNN/utils.py
+++ MTNN/utils.py
@@ -1,24 +1,10 @@
-# The MIT License (MIT)
-# Copyright (c) 2016 Vladimir Ignatev
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the Software
-# is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
-# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
-# OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+import pathlib
+import re
+import os
 import sys
-
+import inspect
+import logging
+from pathlib import PurePath, Path
 
 def progressbar(count, total, status=''):
     bar_len = 60
@@ -28,4 +14,99 @@ def progressbar(count, total, status=''):
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
 
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
-    sys.stdout.flush()  # As suggested by Rom Ruben (see: http://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console/27871113#comment50529068_27871113)
+    sys.stdout.flush()
+
+
+def get_caller_filepath():
+    """
+    Gets previous caller's filepath.
+    Returns:
+        clean_caller_filepath <str>: Previous caller's absolute file path
+
+    """
+    # Get the previous caller's stack frame and extract its file path.
+    last_frame_info = inspect.stack()[-1]
+    caller_filepath = last_frame_info[1]  # in python 3.5+, you can use frame_info.filename
+    del last_frame_info  # drop the reference to the stack frame to avoid reference cycles
+
+    # Use regex to get the base filepath.
+    filename_match = re.search("\w*.py$", caller_filepath)
+
+    if type(filename_match) is not None:
+        match = filename_match.group()
+        clean_caller_filepath = caller_filepath.strip(match)
+    return clean_caller_filepath
+
+
+def get_caller_filename():
+    """
+    Gets previous caller's filepath.
+    Returns:
+        prev_caller_filepath <str>: Previous caller's filename
+
+    """
+    # Get the previous caller's stack frame and extract its file path
+    last_frame_info = inspect.stack()[-1]
+    caller_filepath = last_frame_info[1]  # in python 3.5+, you can use frame_info.filename
+    del last_frame_info  # drop the reference to the stack frame to avoid reference cycles
+    prev_caller_filename = os.path.basename(caller_filepath).strip(".py")
+    #prev_caller_filename = pathlib.PurePath(caller_filepath)
+    return prev_caller_filename
+
+
+def get_logger(logger_name, create_file=False):
+    """
+    Set up logger for each module.
+    Args:
+        logger_name: Pass the module's __name__
+        create_file:
+
+    Returns:
+
+    """
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level=logging.INFO)  # Default
+
+    # Formatter
+    formatter = logging.Formatter()
+
+    #File Handler
+    if create_file:
+
+        filepath = get_caller_filepath()  + "/log" + get_caller_filename() + ".log"
+        print(filepath)
+        fh = logging.FileHandler(get_caller_filename() + ".log")
+        fh.setLevel(level=logging.DEBUG)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    # Console Handler
+    ch = logging.StreamHandler()
+    ch.setLevel(level=logging.ERROR)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger
+
+
+
+def make_path(dir, filename):
+    try:
+        # Check exists
+        if not Path(dir).exists():
+            Path(dir).mkdir()
+
+        if not Path(filename).exists():
+            cleanpath = Path(filename).resolve()
+            print(cleanpath)
+
+    except IOError or FileNotFoundError as exc:
+        print(exc)
+
+    return cleanpath
+
+
+
+def plot():
+    #TODO: Fill in
+    pass
