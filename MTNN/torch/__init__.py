1bed4a3b9b164869c91b4431dea90c18657f9f79
Mao, Christina <mao6@llnl.gov>
2020-05-08 12:06:40 -0700

    Remove torch/

--- MTNN/torch/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Torch Package
-Stores PyTorch Datasets and Functions used by Model
-Parameters in YAML configuration files must match keys provided
-Note: Keys are defined by their respective PyTorch Function Name
-# TODO: Make keys case-insensitive
-"""
--- MTNN/torch/datasets.py
+++
@@ -1,7 +0,0 @@
-"""
-Torch Datasets
-See docs: https://pytorch.org/docs/stable/nn.html#loss-functions
-"""
-
-import torchvision.datasets as torch_datasets
-
