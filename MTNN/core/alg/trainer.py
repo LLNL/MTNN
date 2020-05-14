799517792e26751451f1b503309b140c92620f2c
Mao, Christina <mao6@llnl.gov>
2020-05-13 17:25:35 -0700

    Fix default save path in trainer.py

--- MTNN/core/alg/trainer.py
+++ MTNN/core/alg/trainer.py
@@ -1,33 +1,41 @@
 """
 Trainer
 """
-# standard
-from pathlib import Path, PurePath
-
 # local
 import MTNN.utils as utils
 
-# TODO: Add default save path with filename + date
-DEFAULT_SAVE_DIR = PurePath.joinpath(Path.cwd(), Path("./model/"))
-
+default_save_path = utils.make_default_path(dir= "models", ext= ".pt")
 
 class MultigridTrainer:
     """
     Takes a model and applies some optimizer.
     """
     def __init__(self, dataloader, verbose=False, save=False,
-                 save_path="", load=False, load_path=""):
+                 save_path=default_save_path, load=False, load_path=""):
         self.dataloader = dataloader
         self.verbose = verbose
         self.save = save
-        self.save_path = utils.make_fullpath(DEFAULT_SAVE_DIR.joinpath(save_path).absolute())
+        self.save_path = save_path
         self.load = load
         self.load_path = load_path
 
     def train(self, model, optimizer, cycles):
+        """
+
+        Args:
+            model: <MTNN.BaseModel>
+            optimizer: <MTNN.core.optimizer.multigrid.BaseMultigridHierarchy>
+            cycles: <int> Number of iterations per level
+
+        Returns:
+
+        """
+
+
         for i in range(cycles):
             if self.verbose:
                 print(f"Cycle {i + 1}/{cycles}")
+
             trained_model = optimizer.run(model, self)
 
         return trained_model
