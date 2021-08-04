Refactor to enable computation of tau corrector after my previous refactor. Made a bunch of progress but not ready yet, still a bug altering something when tau is computed.

MTNN/core/components/models.py
@@ -13,6 +13,7 @@ import numpy as np
 
 # local
 from MTNN.utils import logger, printer, deviceloader
+from MTNN.utils.datatypes import ParamVector
 
 log = logger.get_logger(__name__, write_to_file = True)
 
@@ -42,41 +43,47 @@ class _BaseModel(nn.Module):
         """Overwrite this"""
         raise NotImplementedError
 
-    def getGrad(self, dataloader, loss_fn, verbose=False):
-        """Get gradients w.r.t to the entire dataset """
-        # Compute the gradient
-        self.zero_grad()   # zero the parameter gradient
-        total_loss = None
-        for i, mini_batch_data in enumerate(dataloader, 0):
-            # get the inputs. data is a list of [inputs, labels]
-            inputs, labels = deviceloader.load_data(mini_batch_data, self.device)
+    # def getGrad(self, dataloader, loss_fn, verbose=False):
+    #     """Get gradients w.r.t to the entire dataset """
+    #     # Compute the gradient
+    #     self.zero_grad()   # zero the parameter gradient
+    #     total_loss = None
+    #     for i, mini_batch_data in enumerate(dataloader, 0):
+    #         # get the inputs. data is a list of [inputs, labels]
+    #         inputs, labels = deviceloader.load_data(mini_batch_data, self.device)
 
-            if len(inputs.size()) == 4:
-                inputs = inputs.view(inputs.size()[0], inputs.size()[1] * inputs.size()[2] * inputs.size()[3])
+    #         if len(inputs.size()) == 4:
+    #             inputs = inputs.view(inputs.size()[0], inputs.size()[1] * inputs.size()[2] * inputs.size()[3])
 
-            # forward: get the loss w.r.t this batch
-            outputs = self(inputs)
+    #         # forward: get the loss w.r.t this batch
+    #         outputs = self(inputs)
 
-            if total_loss is None:
-                total_loss = loss_fn(outputs, labels)
-            else:
-                total_loss += loss_fn(outputs, labels)
+    #         if total_loss is None:
+    #             total_loss = loss_fn(outputs, labels)
+    #         else:
+    #             total_loss += loss_fn(outputs, labels)
 
-        total_loss.backward()
+    #     total_loss.backward()
 
-        # TODO: Print L2 regularization?
-        """
-        if verbose:
-            printer.printGradNorm(loss)
-        """
+    #     # TODO: Print L2 regularization?
+    #     """
+    #     if verbose:
+    #         printer.printGradNorm(loss)
+    #     """
 
-        weight_grad, bias_grad = [], []
-        for layer_id in range(len(self.layers)):
-            weight_grad.append(self.layers[layer_id].weight.grad.detach().clone())
-            bias_grad.append(self.layers[layer_id].bias.grad.detach().clone())
-        Grad = namedtuple('grad', ['weight_grad', 'bias_grad'])
-        grad = Grad(weight_grad, bias_grad)
-        return grad
+    #     # weight_grad, bias_grad = [], []
+    #     # for layer_id in range(len(self.layers)):
+    #     #     weight_grad.append(self.layers[layer_id].weight.grad.detach().clone())
+    #     #     bias_grad.append(self.layers[layer_id].bias.grad.detach().clone())
+    #     # Grad = namedtuple('grad', ['weight_grad', 'bias_grad'])
+    #     # grad = Grad(weight_grad, bias_grad)
+    #     # return grad
+
+    #     W_grad_array, B_grad_array = [], []
+    #     for layer_id in range(len(self.layers)):
+    #         W_grad_array.append(self.layers[layer_id].weight.grad.detach().clone())
+    #         B_grad_array.append(self.layers[layer_id].bias.grad.detach().clone())
+    #     return ParamVector(W_grad_array, B_grad_array)
 
     def print(self, mode='light'):
         # TODO: Improve modality

MTNN/core/multigrid/operators/SecondOrderConverter.py
@@ -4,130 +4,22 @@ Restriction Operators
 import torch
 import torch.nn as nn
 import numpy as np
-import collections as col
 import copy
 from abc import ABC, abstractmethod
 
 # local
 import MTNN.core.multigrid.scheme as mg
 import MTNN.core.components.models as models
-from MTNN.utils.datatypes import operators
+from MTNN.utils.datatypes import operators, ParamVector
 import MTNN.utils.logger as log
 import MTNN.utils.printer as printer
 import MTNN.utils.deviceloader as deviceloader
-from MTNN.core.multigrid.operators.SecondOrderRestriction import ParamLibrary
 
 log = log.get_logger(__name__, write_to_file =True)
 
 __all__ = ['SecondOrderConverter',
            'MultilinearConverter',
-           'ConvolutionalConverter']
-
-############################################
-# Extractors
-#############################################
-
-class ParameterExtractor:
-    """ParameterExtractor.
-
-    This class extracts parameter tensors from a neural network to a
-    ParamLibrary for restriction/prolongation processing.
-    """
-    def __init__(self, converter):
-        self.converter = converter
-    
-    def extract_from_network(self, level):
-        """Pull parameters out of a network.
-
-        Input: <Level>.
-
-        Output: <tuple(ParamLibrary, ParamLibrary)>. The first
-        ParamLibrary represents network parameters. The second
-        represents momentum values.
-
-        """
-        net = level.net
-        optimizer = level.presmoother.optimizer
-
-        # Pull parameters from the network
-        W_array = [net.layers[layer_id].weight.detach() for layer_id in range(len(net.layers))]
-        B_array = [net.layers[layer_id].bias.detach().reshape(-1, 1) for layer_id in range(len(net.layers))]
-
-        # Pull momentum values from the optimizer.
-        get_p = lambda ind : optimizer.state[optimizer.param_groups[0]['params'][ind]]['momentum_buffer']
-        mW_array, mB_array = [list(x) for x in zip(*[(get_p(2*i), get_p(2*i+1).reshape(-1, 1)) for i in
-                                                     range(int(len(optimizer.param_groups[0]['params']) / 2))])]
-
-        param_library, momentum_library = (ParamLibrary(W_array, B_array), ParamLibrary(mW_array, mB_array))
-        self.converter.convert_network_format_to_MTNN(param_library, momentum_library)
-        return param_library, momentum_library
-
-    def insert_into_network(self, level, param_library, momentum_library):
-        """Insert ParamLibrary tensors into a network.
-
-        Inputs:
-        level <Level>. The level into which to insert the parameters.
-        net_param_library <ParamLibrary>. The tensors of network parameters to insert.
-        momentum_library <ParamLibrary>. The tensors of momentum values to insert.
-
-        Output: None.
-        """
-        # TODO: This function uses two parameter copies which is
-        # unnecessary. Refactor to only use one.
-        level.init_params = copy.deepcopy(param_library)
-        level.init_momentum = copy.deepcopy(momentum_library)
-
-        self.converter.convert_MTNN_format_to_network(param_library, momentum_library)
-        W_array, B_array = param_library
-        mW_array, mB_array = momentum_library
-
-        # Insert parameters into the network
-        with torch.no_grad():
-            for layer_id in range(len(level.net.layers)):
-                level.net.layers[layer_id].weight.copy_(W_array[layer_id])
-                level.net.layers[layer_id].bias.copy_(B_array[layer_id].reshape(-1))
-        level.net.zero_grad()
-
-        # Insert momemntum values into the optimizer
-        level.presmoother.momentum_data = []
-        with torch.no_grad():
-            for i in range(len(mW_array)):
-                level.presmoother.momentum_data.append(mW_array[i].clone())
-                level.presmoother.momentum_data.append(mB_array[i].clone().reshape(-1))
-        level.presmoother.optimizer = None
-
-    def add_to_network(self, level, param_diff_library, momentum_diff_library):
-        """Add ParamLibrary difference value tensors to a network.
-
-        This is similar to insert_into_network, except that it does not
-        replace the values of the network but instead adds the inputs
-        to the existing values. This is done, for example, in a Full
-        Approximation Scheme coarse-grid correction.
-
-        Inputs:
-        level <Level>. The level into which to insert the parameters.
-        net_param_library <ParamLibrary>. The tensors of network parameters to insert.
-        momentum_library <ParamLibrary>. The tensors of momentum values to insert.
-
-        Output: None.
-
-        """
-        self.converter.convert_MTNN_format_to_network(param_diff_library, momentum_diff_library)
-        dW_array, dB_array = param_diff_library
-        dmW_array, dmB_array = momentum_diff_library
-
-        optimizer = level.presmoother.optimizer
-        get_p = lambda ind : optimizer.state[optimizer.param_groups[0]['params'][ind]]['momentum_buffer']
-
-        with torch.no_grad():
-            for layer_id in range(len(level.net.layers)):
-                # Add parameters to the network
-                level.net.layers[layer_id].weight.add_(dW_array[layer_id])
-                level.net.layers[layer_id].bias.add_(dB_array[layer_id].reshape(-1))
-                # Add momentum values to the optimizer.
-                get_p(2*layer_id).add_(dmW_array[layer_id])
-                get_p(2*layer_id+1).add_(dmB_array[layer_id].reshape(-1))
-    
+           'ConvolutionalConverter']    
 
 ############################################
 # Converters
@@ -136,7 +28,7 @@ class ParameterExtractor:
 class SecondOrderConverter:
     """Converts parameter libraries between format.
 
-    A ParamLibrary contains lists of weight and bias tensors. The
+    A ParamVector contains lists of weight and bias tensors. The
     format of these tensors as required by the neural networks is
     different than the format required by torch for its matrix
     multiplication broadcasting semantics, which we use during
@@ -146,30 +38,30 @@ class SecondOrderConverter:
     """
     
     @abstractmethod
-    def convert_network_format_to_MTNN(self, param_library, momentum_library):
-        """ Convert tensors in a ParamLibrary from network format to MTNN in place.
+    def convert_network_format_to_MTNN(self, param_vector):
+        """ Convert tensors in a ParamVector from network format to MTNN in place.
 
         The network format is the tensor format of the weight and bias
         tensors as used by the neural network. The MTNN format is
         the tensor format appropriate for numerical restriction and
         prolongation linear algebra based computation.
 
-        Input: <ParamLibrary>
+        Input: <ParamVector>
         Output: None
         """
         raise NotImplementedError
 
     
     @abstractmethod
-    def convert_MTNN_format_to_network(self, param_library, momentum_library):
-        """ Convert tensors in a ParamLibrary from MTNN format to network format in place.
+    def convert_MTNN_format_to_network(self, param_vector):
+        """ Convert tensors in a ParamVector from MTNN format to network format in place.
 
         The network format is the tensor format of the weight and bias
         tensors as used by the neural network. The MTNN format is
         the tensor format appropriate for numerical restriction and
         prolongation linear algebra based computation.
 
-        Input: <ParamLibrary>
+        Input: <ParamVector>
         Output: None
         """
         raise NotImplementedError
@@ -180,10 +72,10 @@ class MultiLinearConverter(SecondOrderConverter):
 
     A SecondOrderConverter for multilinear, aka fully-connected, models.
     """
-    def convert_network_format_to_MTNN(self, param_library, momentum_library):
+    def convert_network_format_to_MTNN(self, param_vector):
         pass
 
-    def convert_MTNN_format_to_network(self, param_library, momentum_library):
+    def convert_MTNN_format_to_network(self, param_vector):
         pass
 
 
@@ -197,15 +89,13 @@ class ConvolutionalConverter(SecondOrderConverter):
     def __init__(self, num_conv_layers):
         self.num_conv_layers = num_conv_layers
     
-    def convert_network_format_to_MTNN(self, param_library, momentum_library):
+    def convert_network_format_to_MTNN(self, param_vector):
         # Reorder convolutional tensors
         for layer_id in range(self.num_conv_layers):
             # Has shape (out_ch x in_ch x kernel_d1 x kernel_d2 x ... x kernel_dk)
-            W = param_library.weights[layer_id]
-            mW = momentum_library.weights[layer_id]
+            W = param_vector.weights[layer_id]
             # Convert to shape (kernel_d1 x kernel_d2 x ... x kernel_dk x out_ch x in_ch)
-            param_library.weights[layer_id] = W.permute(*range(2, len(W.shape)), 0, 1)
-            momentum_library.weights[layer_id] = mW.permute(*range(2, len(W.shape)), 0, 1)
+            param_vector.weights[layer_id] = W.permute(*range(2, len(W.shape)), 0, 1)
 
         # Each neuron in the first FC layer after convolutional layers
         # has a separate weight for each pixel and channel. For each
@@ -215,9 +105,8 @@ class ConvolutionalConverter(SecondOrderConverter):
         # convert this weight matrix into a 3rd order tensor with
         # dimensions (# pixels, # neurons, # last_layer_channnels).
         layer_id = self.num_conv_layers
-        last_layer_channels = param_library.weights[layer_id-1].shape[-2]
-        W = param_library.weights[layer_id]
-        mW = momentum_library.weights[layer_id]
+        last_layer_channels = param_vector.weights[layer_id-1].shape[-2]
+        W = param_vector.weights[layer_id]
 
         # Last conv layer output has shape (minibatches, out_ch,
         # pixel_rows, pixel_cols) and gets flattened to shape
@@ -227,35 +116,27 @@ class ConvolutionalConverter(SecondOrderConverter):
         
         # Creates tensor of shape (neurons, last_layer_channels, # pixels)
         Wnew = W.reshape(W.shape[0], last_layer_channels, int(W.shape[1] / last_layer_channels))
-        mWnew = mW.reshape(mW.shape[0], last_layer_channels, int(mW.shape[1] / last_layer_channels))
         # Permute to correct shape
         Wnew = Wnew.permute(2, 0, 1)
-        mWnew = mWnew.permute(2, 0, 1)
 
-        param_library.weights[layer_id] = Wnew
-        momentum_library.weights[layer_id] = mWnew
+        param_vector.weights[layer_id] = Wnew
     
-    def convert_MTNN_format_to_network(self, param_library, momentum_library):
+    def convert_MTNN_format_to_network(self, param_vector):
         # Reorder convolutional tensors
         for layer_id in range(self.num_conv_layers):
             # Has shape (kernel_d1 x kernel_d2 x ... x kernel_dk x out_ch x in_ch)
-            W = param_library.weights[layer_id]
-            mW = momentum_library.weights[layer_id]
+            W = param_vector.weights[layer_id]
             # Convert to shape (out_ch x in_ch x kernel_d1 x kernel_d2 x ... x kernel_dk)
-            param_library.weights[layer_id] = W.permute(-2, -1, *range(len(W.shape)-2))
-            momentum_library.weights[layer_id] = mW.permute(-2, -1, *range(len(W.shape)-2))
+            param_vector.weights[layer_id] = W.permute(-2, -1, *range(len(W.shape)-2))
 
         # First FC layer has shape (# pixels, out_neurons,
         # last_layer_channels).  Needs to have shape (out_neurons,
         # all_input_values) and needs to maintain "channel-major"
         # ordering.
         layer_id = self.num_conv_layers
-        W = param_library.weights[layer_id]
+        W = param_vector.weights[layer_id]
         W = torch.flatten(W.permute(1, 2, 0), 1)
-        mW = momentum_library.weights[layer_id]
-        mW = torch.flatten(mW.permute(1, 2, 0), 1)
-        param_library.weights[layer_id] = W
-        momentum_library.weights[layer_id] = mW
+        param_vector.weights[layer_id] = W
 
         
 class ActivationDistanceConverter(SecondOrderConverter):
@@ -287,11 +168,11 @@ class ActivationDistanceConverter(SecondOrderConverter):
         self.half_for_average = half_for_average
 
     def convert_biases_to_activation_distances(self, library):
-        """ Convert biases in a ParamLibrary into activation distances.
+        """ Convert biases in a ParamVector into activation distances.
         Place activation distance in what is normally the bias spot.
 
         Inputs:
-        library (ParamLibrary)
+        library (ParamVector)
         """
         for layer_id in range(len(library.weights)):
             W = library.weights[layer_id]
@@ -311,10 +192,10 @@ class ActivationDistanceConverter(SecondOrderConverter):
         self.convert_biases_to_activation_distances(momentum_library)
 
     def convert_activation_distances_to_biases(self, library):
-        """ Convert activation distances in a ParamLibrary back into biases.
+        """ Convert activation distances in a ParamVector back into biases.
 
         Inputs:
-        library (ParamLibrary)
+        library (ParamVector)
         """
         for layer_id in range(len(library.weights)):
             W = library.weights[layer_id]
@@ -332,3 +213,92 @@ class ActivationDistanceConverter(SecondOrderConverter):
         self.convert_activation_distances_to_biases(param_library)
         self.convert_activation_distances_to_biases(momentum_library)
         self.primary_converter.convert_MTNN_format_to_network(param_library, momentum_library)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class ConvolutionalConverter(SecondOrderConverter):
+#     """ConvolutionalConverter
+
+#     A SecondOrderConverter for convolutional networks that consist of
+#     1 or more convolutional layers followed by 0 or more
+#     fully-connected layers.
+#     """
+#     def __init__(self, num_conv_layers):
+#         self.num_conv_layers = num_conv_layers
+    
+#     def convert_network_format_to_MTNN(self, param_library, momentum_library):
+#         # Reorder convolutional tensors
+#         for layer_id in range(self.num_conv_layers):
+#             # Has shape (out_ch x in_ch x kernel_d1 x kernel_d2 x ... x kernel_dk)
+#             W = param_library.weights[layer_id]
+#             mW = momentum_library.weights[layer_id]
+#             # Convert to shape (kernel_d1 x kernel_d2 x ... x kernel_dk x out_ch x in_ch)
+#             param_library.weights[layer_id] = W.permute(*range(2, len(W.shape)), 0, 1)
+#             momentum_library.weights[layer_id] = mW.permute(*range(2, len(W.shape)), 0, 1)
+
+#         # Each neuron in the first FC layer after convolutional layers
+#         # has a separate weight for each pixel and channel. For each
+#         # pixel, there is a set of columns in the FC weight matrix
+#         # associated with that pixel's channels, and we need to merge
+#         # those columns acoording to our coarsening. Thus, we need to
+#         # convert this weight matrix into a 3rd order tensor with
+#         # dimensions (# pixels, # neurons, # last_layer_channnels).
+#         layer_id = self.num_conv_layers
+#         last_layer_channels = param_library.weights[layer_id-1].shape[-2]
+#         W = param_library.weights[layer_id]
+#         mW = momentum_library.weights[layer_id]
+
+#         # Last conv layer output has shape (minibatches, out_ch,
+#         # pixel_rows, pixel_cols) and gets flattened to shape
+#         # (minibatches, outch * pixels_rows * pixel_cols), which keeps
+#         # last indexed elements together. That is, it's a sort of
+#         # "channel-major order."
+        
+#         # Creates tensor of shape (neurons, last_layer_channels, # pixels)
+#         Wnew = W.reshape(W.shape[0], last_layer_channels, int(W.shape[1] / last_layer_channels))
+#         mWnew = mW.reshape(mW.shape[0], last_layer_channels, int(mW.shape[1] / last_layer_channels))
+#         # Permute to correct shape
+#         Wnew = Wnew.permute(2, 0, 1)
+#         mWnew = mWnew.permute(2, 0, 1)
+
+#         param_library.weights[layer_id] = Wnew
+#         momentum_library.weights[layer_id] = mWnew
+    
+#     def convert_MTNN_format_to_network(self, param_library, momentum_library):
+#         # Reorder convolutional tensors
+#         for layer_id in range(self.num_conv_layers):
+#             # Has shape (kernel_d1 x kernel_d2 x ... x kernel_dk x out_ch x in_ch)
+#             W = param_library.weights[layer_id]
+#             mW = momentum_library.weights[layer_id]
+#             # Convert to shape (out_ch x in_ch x kernel_d1 x kernel_d2 x ... x kernel_dk)
+#             param_library.weights[layer_id] = W.permute(-2, -1, *range(len(W.shape)-2))
+#             momentum_library.weights[layer_id] = mW.permute(-2, -1, *range(len(W.shape)-2))
+
+#         # First FC layer has shape (# pixels, out_neurons,
+#         # last_layer_channels).  Needs to have shape (out_neurons,
+#         # all_input_values) and needs to maintain "channel-major"
+#         # ordering.
+#         layer_id = self.num_conv_layers
+#         W = param_library.weights[layer_id]
+#         W = torch.flatten(W.permute(1, 2, 0), 1)
+#         mW = momentum_library.weights[layer_id]
+#         mW = torch.flatten(mW.permute(1, 2, 0), 1)
+#         param_library.weights[layer_id] = W
+#         momentum_library.weights[layer_id] = mW

MTNN/core/multigrid/operators/SecondOrderRestriction.py
@@ -3,7 +3,6 @@ Restriction Operators
 """
 import torch
 import torch.nn as nn
-import numpy as np
 import collections as col
 from abc import ABC, abstractmethod
 
@@ -11,7 +10,7 @@ from abc import ABC, abstractmethod
 import MTNN.core.multigrid.scheme as mg
 #import MTNN.core.multigrid.operators.interpolator as interp
 import MTNN.core.components.models as models
-from MTNN.utils.datatypes import operators
+from MTNN.utils.datatypes import operators, ParamVector
 import MTNN.utils.logger as log
 import MTNN.utils.printer as printer
 import MTNN.utils.deviceloader as deviceloader
@@ -21,111 +20,6 @@ log = log.get_logger(__name__, write_to_file =True)
 __all__ = ['SecondOrderRestriction',
            'SecondOrderProlongation']
 
-############################################
-# Data types
-#############################################
-
-"""ParamLibrary - a data store of parameters associated with a neural network
-
-weights_list and bias_list are each lists of length equal to the
-number of layers in a network.
-
-Each element of weights_list is a tensor of order >=2 such that
-dimension -2 is the number of output channels and dimension -1 is the
-number of input channels. Note that for fully-connected layers, this
-tensor will be of order 2 exactly, in which case dimension -2 is the
-rows and dimension -1 is the columns.
-
-Each element of bias_list is a tensor of order 1 of length equal to
-the number of output channels.
-"""
-ParamLibrary = col.namedtuple("ParamLibrary", "weights biases")
-
-
-"""CoarseMapping - specifies a mapping from fine channels to coarse channels.
-
-fine2coarse_map <List(List)> - A list of length equal to the number of
-network layers. Each element is itself a list, of length equal to the
-number of channels in the fine-level network. Each element specifies
-the index of the coarse channel to which this fine channel is mapped.
-
-num_coarse_channels <List> - A list of length equal to the number of
-network layers. Each element contains the number of coarse channels at
-that layer.
-"""
-CoarseMapping = col.namedtuple("CoarseMapping", "fine2coarse_map, num_coarse_channels")
-
-
-"""TransferOps - The matrix operators used in restriction and prolongation.
-
-R_ops <List> - A list, of length equal to the number of layers minus
-1, containing the R matrices used in restriction.
-
-P_ops <List> - A list, of length equal to the number of layers minus
-1, containing the P matrices used in restriction.
-
-R_for_grad_ops <List> - A list, of length equal to the number of
-layers minus 1, containing the R matrices used in restriction of
-gradients in a tau correction.
-
-P_for_grad_ops <List> - A list, of length equal to the number of
-layers minus 1, containing the P matrices used in restriction of
-gradients in a tau correction.
-"""
-TransferOps = col.namedtuple("TransferOps", "R_ops P_ops R_for_grad_ops P_for_grad_ops")
-
-
-####################################################################
-# Transfer Operator Templates
-####################################################################
-
-def transfer(Wmats, Bmats, R_ops, P_ops):
-    # Matrix multiplication broadcasting:
-    # T of shape (a, b, c, N, M)
-    # Matrix A of shape (k, N)
-    # Matrix B of shape (M, p)
-    # A @ T @ B computes a tensor of shape (a, b, c, k, p) such that
-    # (i, j, l, :, :) = A @ T(i,j,l,:,:) @ B
-    num_layers = len(Wmats)
-    Wdest_array = []
-    Bdest_array = []
-    for layer_id in range(num_layers):
-        Wsrc = Wmats[layer_id]
-        Bsrc = Bmats[layer_id]
-        if layer_id < num_layers - 1:
-            if layer_id == 0:
-                Wdest = R_ops[layer_id] @ Wsrc
-            else:
-                Wdest = R_ops[layer_id] @ Wsrc @ P_ops[layer_id - 1]
-            Bdest = R_ops[layer_id] @ Bsrc
-        elif layer_id > 0:            
-            Wdest = Wsrc @ P_ops[layer_id-1]
-            Bdest = Bsrc.clone()
-
-        Wdest_array.append(Wdest)
-        Bdest_array.append(Bdest)
-    return Wdest_array, Bdest_array
-
-def transfer_star(Wmats, Bmats, R_ops, P_ops):
-    num_layers = len(Wmats)
-    Wdest_array = []
-    Bdest_array = []
-    for layer_id in range(num_layers):
-        Wsrc = Wmats[layer_id]
-        Bsrc = Bmats[layer_id]
-        if layer_id < num_layers - 1:
-            if layer_id == 0:
-                Wdest = R_ops[layer_id] * Wsrc
-            else:
-                Wdest = R_ops[layer_id] * Wsrc * P_ops[layer_id - 1]
-            Bdest = R_ops[layer_id] * Bsrc
-        elif layer_id > 0:
-            Wdest = Wsrc * P_ops[layer_id-1]
-            Bdest = Bsrc.clone()
-
-        Wdest_array.append(Wdest)
-        Bdest_array.append(Bdest)
-    return Wdest_array, Bdest_array
 
 ####################################################################
 # Restriction
@@ -151,11 +45,11 @@ class SecondOrderRestriction:
         Inputs: 
         parameter_extractor <ParameterExtractor>
 
-        matching_method <Callable>. Takes as input a ParamLibrary and
+        matching_method <Callable>. Takes as input a ParamVector and
         a Level, produces as output a CoarseMapping object.
 
         transfer_operator_builder <Callable>. Takes as input a
-        ParamLibrary, a CoarseMapping, and a torch.device, and
+        ParamVector, a CoarseMapping, and a torch.device, and
         produces a TransferOps object.
 
         adjust_bias <bool>. Whether or not to adjust the coarse biases
@@ -200,32 +94,13 @@ class SecondOrderRestriction:
 
     def apply(self, fine_level, coarse_level, dataloader, verbose=False):
         fine_param_library, fine_momentum_library = self.parameter_extractor.extract_from_network(fine_level)
-
         coarse_mapping = self.matching_method(fine_param_library, fine_level.net)
 
-        self.transfer_ops = self.transfer_operator_builder(fine_param_library,
-                                                           coarse_mapping,
-                                                           deviceloader.get_device())
-        R_ops = self.transfer_ops.R_ops
-        P_ops = self.transfer_ops.P_ops
-
-        W_f_array, B_f_array = fine_param_library
-        W_c_array, B_c_array = transfer(W_f_array, B_f_array, R_ops, P_ops)
-#        B_c_array[0] *= 1.04
-
-        mW_f_array, mB_f_array = fine_momentum_library
-        mW_c_array, mB_c_array = transfer(mW_f_array, mB_f_array, R_ops, P_ops)
-
-#        print(B_c_array[0])
-#         adjustments_array = self.get_bias_adjustments(W_f_array, B_f_array, W_c_array, B_c_array, coarse_mapping)
-#         for layer_id in range(len(adjustments_array)):
-#             B_c_array[layer_id] = B_c_array[layer_id] * adjustments_array[layer_id]
-# #            print(-B_c_array[layer_id] / torch.norm(W_c_array[layer_id], p=2, dim=1, keepdim=True))
-#             mB_c_array[layer_id] = mB_c_array[layer_id] * adjustments_array[layer_id]
-# #        print(B_c_array[0])
-
-        coarse_param_library = ParamLibrary(W_c_array, B_c_array)
-        coarse_momentum_library = ParamLibrary(mW_c_array, mB_c_array)
+        self.transfer_ops, self.tau_transfer_ops = self.transfer_operator_builder(fine_param_library, 
+                                                                                  coarse_mapping,
+                                                                                  deviceloader.get_device())
+        coarse_param_library = self.transfer_ops @ fine_param_library
+        coarse_momentum_library = self.transfer_ops @ fine_momentum_library
 
         # So this is some terrible software design right here. :-D
         # TODO: Refactor for architecture extensibility.
@@ -241,8 +116,8 @@ class SecondOrderRestriction:
             out_ch, kernel_widths, strides = zip(*fine_level.net.conv_channels)
             out_ch = [out_ch[0]] + coarse_mapping.num_coarse_channels[:num_conv_layers]
             conv_channels = list(zip(out_ch, kernel_widths, strides))
-            fc_dims = [conv_channels[-1][0] * W_c_array[num_conv_layers].shape[0]] + \
-                      coarse_mapping.num_coarse_channels[num_conv_layers:] + \
+            first_fc_width = conv_channels[-1][0] * coarse_param_library.weights[num_conv_layers].shape[0]
+            fc_dims = [first_fc_width] + coarse_mapping.num_coarse_channels[num_conv_layers:] + \
                       [fine_level.net.layers[-1].out_features]
             coarse_level.net = fine_level.net.__class__(conv_channels,
                                                         fc_dims,
@@ -253,6 +128,8 @@ class SecondOrderRestriction:
         
         self.parameter_extractor.insert_into_network(coarse_level, coarse_param_library,
                                                      coarse_momentum_library)
+
+#        coarse_level.corrector.compute_tau(coarse_level, fine_level, dataloader, self.transfer_ops)
 
 ####################################################################
 # Prolongation
@@ -278,29 +155,14 @@ class SecondOrderProlongation:
     def apply(self, fine_level, coarse_level, dataloader, verbose):
         assert(fine_level.id < coarse_level.id)
 
-        R_ops = self.restriction.transfer_ops.R_ops
-        P_ops = self.restriction.transfer_ops.P_ops
-
         coarse_param_library, coarse_momentum_library = self.parameter_extractor.extract_from_network(coarse_level)
-        W_c_array, B_c_array = coarse_param_library
-        mW_c_array, mB_c_array = coarse_momentum_library
-        c_init_params = coarse_level.init_params
-        c_init_momentum = coarse_level.init_momentum
-        eW_array = []
-        eB_array = []
-        emW_array = []
-        emB_array = []
-        for layer_id in range(len(W_c_array)):
-#            print("{}: W_c and Winit shapes are {}, {}"
-#                  .format(layer_id, W_c_array[layer_id].shape, c_init_params.weights[layer_id].shape))
-            eW_array.append(W_c_array[layer_id] - c_init_params.weights[layer_id])
-            eB_array.append(B_c_array[layer_id] - c_init_params.biases[layer_id])
-            emW_array.append(mW_c_array[layer_id] - c_init_momentum.weights[layer_id])
-            emB_array.append(mB_c_array[layer_id] - c_init_momentum.biases[layer_id])
-        eW_array, eB_array = transfer(eW_array, eB_array, P_ops, R_ops)
-        emW_array, emB_array = transfer(emW_array, emB_array, P_ops, R_ops)
+        prolongation_ops = self.restriction.transfer_ops.swap_transfer_ops()
 
-        fine_param_diff_library = ParamLibrary(eW_array, eB_array)
-        fine_momentum_diff_library = ParamLibrary(emW_array, emB_array)
+        coarse_param_diff_library = coarse_param_library - coarse_level.init_params
+        coarse_momentum_diff_library = coarse_momentum_library - coarse_level.init_momentum
+
+        fine_param_diff_library = prolongation_ops @ coarse_param_diff_library
+        fine_momentum_diff_library = prolongation_ops @ coarse_momentum_diff_library
+
         self.parameter_extractor.add_to_network(fine_level, fine_param_diff_library,
                                                 fine_momentum_diff_library)

MTNN/core/multigrid/operators/SimilarityMatcher.py
@@ -6,7 +6,7 @@ import pdb
 #local
 from MTNN.utils import logger
 from MTNN.utils import deviceloader
-from MTNN.core.multigrid.operators.SecondOrderRestriction import CoarseMapping
+from MTNN.utils.datatypes import CoarseMapping
 log = logger.get_logger(__name__, write_to_file =True)
 
 class StandardSimilarity:
@@ -97,15 +97,17 @@ class HEMCoarsener():
     Takes a fine-level and constructs the coarsening layer to be used
     for building the restriction operator
     """
-    def __init__(self, similarity_calculator):
+    def __init__(self, similarity_calculator, coarsen_on_layer = None):
         """
         Args:
-            theta:
-            randseq:
+            similarity_calculator (class) Class that measures similarity between two neurons.
+            coarsen_on_layer (array[bool]) For each layer, if False, return identity matching. 
+                                           If arg is None, coarsen every layer.
         """
         self.similarity_calculator = similarity_calculator
         self.coarseLevelDim = None
         self.Fine2CoarsePerLayer = None
+        self.coarsen_on_layer = coarsen_on_layer
 
     def get_heavyedgematching(self, similarityMatrix, random_seq=False):
         threshold = 0.0
@@ -153,7 +155,7 @@ class HEMCoarsener():
     def __call__(self, param_matrix_list, net):
         """
         """
-        W_array, B_array = param_matrix_list
+        W_array, B_array = param_matrix_list.weights, param_matrix_list.biases
         
         num_layers = len(W_array)
         # number columns
@@ -169,16 +171,20 @@ class HEMCoarsener():
             wb = torch.cat([w, b], dim=1)
 
             # f-size (w.shape[0])
-            nf = W_array[0].shape[0]
+            nf = wb.shape[0]
 
-            similarity = self.similarity_calculator.calculate_similarity(wb, net, layer_id)
+            if self.coarsen_on_layer is None or self.coarsen_on_layer[layer_id]:
+                similarity = self.similarity_calculator.calculate_similarity(wb, net, layer_id)
 
-            similarity.fill_diagonal_(-999.0)
-            f2c, num_ColIn = self.get_heavyedgematching(similarity)
-            print("Layer {} has {} coarse neurons".format(layer_id, num_ColIn))
+                similarity.fill_diagonal_(-999.0)
+                f2c, num_ColIn = self.get_heavyedgematching(similarity)
+                num_coarse_array.append(num_ColIn)
+                fine2CoarsePerLayer.append(f2c)
+            else:
+                num_coarse_array.append(nf)
+                fine2CoarsePerLayer.append(torch.arange(0, nf, dtype=int))
+            print("Layer {} has {} coarse neurons".format(layer_id, num_coarse_array[-1]))
             print()
-            num_coarse_array.append(num_ColIn)
-            fine2CoarsePerLayer.append(f2c)
 
         return CoarseMapping(fine2CoarsePerLayer, num_coarse_array)
 

MTNN/core/multigrid/operators/TransferOpsBuilder.py
@@ -5,8 +5,7 @@ import torch
 # local
 import MTNN.core.multigrid.scheme as mg
 import MTNN.utils.logger as log
-import MTNN.utils.datatypes as mgdata
-from MTNN.core.multigrid.operators.SecondOrderRestriction import ParamLibrary, CoarseMapping, TransferOps
+from MTNN.utils.datatypes import ParamVector, CoarseMapping, TransferOps
 
 log = log.get_logger(__name__, write_to_file = True)
 
@@ -47,7 +46,7 @@ class PairwiseOpsBuilder:
     def __call__(self, param_library, coarse_mapping, op_device):
         """
         Inputs:
-        param_library <ParamLibrary> - The parameters associated with the network.
+        param_library <ParamVector> - The parameters associated with the network.
         coarse_mapping <CoarseMapping> - The mapping from fine to coarse channels.
         op_device <torch.device> - The device on which the ops matrices should reside.
         """
@@ -60,7 +59,7 @@ class PairwiseOpsBuilder:
 
         fine2coarse, num_coarse_array = coarse_mapping
         with torch.no_grad():
-            W_f_array, B_f_array = param_library
+            W_f_array, B_f_array = param_library.weights, param_library.biases
             for layer_id in range(len(W_f_array)-1):
                 F2C_layer = fine2coarse[layer_id]
                 nF = len(F2C_layer)
@@ -73,9 +72,9 @@ class PairwiseOpsBuilder:
 
                 if self.weighted_projection:
                     WB = torch.cat((currW, currB), dim=1)
-                    nr = torch.norm(WB, p=2, dim=1, keepdim=True)
+                    nr = torch.norm(WB, p=2, dim=1, keepdim=True).to(op_device)
                 else:
-                    nr = torch.ones(currW.shape[0], 1)
+                    nr = torch.ones(currW.shape[0], 1).to(op_device)
                 
                 R_layer = torch.zeros([nC, nF]).to(op_device)
                 for i in range(nF):
@@ -88,17 +87,7 @@ class PairwiseOpsBuilder:
                 # P = diag(nr) * R^T * diag(1./d)^(1-rwp)
                 P_layer = (torch.transpose(R_layer, 0, 1) * nr) / torch.pow(d, 1.0 - self.rwp).reshape(1, -1)
                 
-                # if self.sum_on_restriction:
-                #     # Construct prolongation operators for each layer
-                #     # TODO - Decouple?
-
-                #     P_layer = (torch.transpose(R_layer, 0, 1) * nr) / d.reshape(1, -1)
-                # else:
-                #     R_layer = R_layer / d.reshape(-1, 1)
-                #     P_layer = torch.transpose(R_layer, 0, 1) * nr
-
                 restriction_operators.append(R_layer)
-
                 prolongation_operators.append(P_layer)
 
                 R_for_grad_layer = torch.transpose(P_layer, 0, 1)
@@ -111,6 +100,5 @@ class PairwiseOpsBuilder:
                 #     bias_restriction_operators(adjustments * R_layer)
                 #     bias_prolongation_operators(P_layer / adjustments)
                     
-        return TransferOps(restriction_operators, prolongation_operators,
-                           restriction_for_grad_operators, prolongation_for_grad_operators)
+        return TransferOps(restriction_operators, prolongation_operators), TransferOps(restriction_for_grad_operators, prolongation_for_grad_operators)
 

examples/darcy_test.py
@@ -17,9 +17,10 @@ from os import path
 
 # local
 from MTNN.core.components import data, models, subsetloader
-from MTNN.core.multigrid.operators import tau_corrector, smoother
+from MTNN.core.multigrid.operators import TauCorrector, smoother
 import MTNN.core.multigrid.operators.SecondOrderRestriction as SOR
 import MTNN.core.multigrid.operators.SecondOrderConverter as SOC
+import MTNN.core.multigrid.operators.ParameterExtractor as PE
 import MTNN.core.multigrid.operators.SimilarityMatcher as SimilarityMatcher
 import MTNN.core.multigrid.operators.TransferOpsBuilder as TransferOpsBuilder
 from MTNN.core.alg import trainer, evaluator
@@ -27,7 +28,8 @@ from MTNN.utils import deviceloader
 import MTNN.core.multigrid.scheme as mg
 
 # Darcy problem imports
-sys.path.append("./data_darcy_multilevel")
+darcy_path = "./datasets/darcy"
+sys.path.append(darcy_path)
 from PDEDataSet import *
 
 def read_args(args):
@@ -74,10 +76,10 @@ torch.set_printoptions(precision=5)
 # Set up data
 #=====================================
 
-train_filename = 'data_darcy_multilevel/train_data_32.npz'
-test_filename = 'data_darcy_multilevel/test_data_32.npz'
+train_filename = darcy_path + '/train_data_32.npz'
+test_filename = darcy_path + '/test_data_32.npz'
 percent_train = 0.8
-orig_filename = 'data_darcy_multilevel/match_pde_data_u_Q_32_50000.npz'
+orig_filename = darcy_path + '/match_pde_data_u_Q_32_50000.npz'
 
 if not path.exists(train_filename):
     print("Generating training and testing files.")
@@ -138,7 +140,7 @@ class SGDparams:
         self.momentum = momentum
         self.l2_decay = l2_decay
 #SGDparams = namedtuple("SGDparams", ["lr", "momentum", "l2_decay"])
-tau = tau_corrector.BasicTau
+tau = TauCorrector.BasicTau
 
 # Build Multigrid Hierarchy Levels/Grids
 num_levels = params["num_levels"]
@@ -160,9 +162,12 @@ for level_idx in range(0, num_levels):
                                         optim_params = optim_params,
                                         log_interval = 1)
 
-    parameter_extractor = SOC.ParameterExtractor(SOC.ConvolutionalConverter(net.num_conv_layers))
-    matching_method = SimilarityMatcher.HEMCoarsener(similarity_calculator=SimilarityMatcher.StandardSimilarity())
-    transfer_operator_builder = TransferOpsBuilder.PairwiseOpsBuilder()
+    converter = SOC.ConvolutionalConverter(net.num_conv_layers)
+    parameter_extractor = PE.ParamMomentumExtractor(converter)
+    gradient_extractor = PE.GradientExtractor(converter)
+    matching_method = SimilarityMatcher.HEMCoarsener(similarity_calculator=SimilarityMatcher.StandardSimilarity(),
+                                                     coarsen_on_layer=None)#[False, False, True, True])
+    transfer_operator_builder = TransferOpsBuilder.PairwiseOpsBuilder(restriction_weighting_power=0.0, weighted_projection=True)
     restriction = SOR.SecondOrderRestriction(parameter_extractor, matching_method, transfer_operator_builder)
     prolongation = SOR.SecondOrderProlongation(parameter_extractor, restriction)
     aLevel = mg.Level(id=level_idx,
@@ -172,7 +177,7 @@ for level_idx in range(0, num_levels):
                       restriction = restriction, #restriction_op(interpolator.PairwiseAggCoarsener),
                       coarsegrid_solver = sgd_smoother,
                       num_epochs = smooth_pattern[level_idx],
-                      corrector = tau(loss_fn))
+                      corrector = tau(loss_fn, gradient_extractor))
 
     FAS_levels.append(aLevel)
 